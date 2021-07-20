014458e96e5c4da8b207a8e900d5bf8124bb4578
Tfauves <tfauvelle@gmail.com>
2021-07-20 11:20:30 -0500

    Defined method behavior within cuo class.

--- src/com/careerDevs/Cup.java
+++ src/com/careerDevs/Cup.java
@@ -4,36 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cup {
-   List<Die> dice = new ArrayList<>();
+    List<Die> dice = new ArrayList<>();
 
-   public Cup() {
-       while(dice.size() < 5) {
-           dice.add(new Die());
-       }
-   }
+    public Cup() {
+        while (dice.size() < 5) {
+            dice.add(new Die());
+        }
+    }
 
-   public void roll() {
-       for (Die die : dice) {
-           die.roll();
-       }
-   }
+    public void roll() {
+        for (Die die : dice) {
+            die.roll();
+        }
+    }
 
-   public void roll(int selection) {
-       dice.get(selection).roll();
-   }
+    public void roll(int selection) {
+        dice.get(selection).roll();
+    }
 
-   public void roll() {
+    public void roll(List<Integer> selections) {
+        for (int seclection : selections) {
+            roll(seclection);
+        }
+    }
 
-   }
+    public String displayCup() {
+        String output = "";
+        for (Die die : dice) {
+            output += die.faceUpValue + " ";
+        }
+        return output.trim();
+    }
 
-   public String displayCup() {
-       String output = "";
-       for (Die die : dice) {
-           output += die.faceUpValue + " ";
-       }
-       return output.trim();
-   }
+    public List<Integer> parseSelections(String input) {
+        String[] inputArr = input.split(" ");
 
+        List<Integer> selections = new ArrayList<>();
+        for (String number : inputArr) {
+            selections.add(Integer.parseInt(number) - 1);
+        }
+        return selections;
+    }
 
 
 }
--- src/com/careerDevs/Main.java
+++ src/com/careerDevs/Main.java
@@ -2,6 +2,8 @@ package com.careerDevs;
 
 import com.HighLow.HighLow;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,15 +12,15 @@ public class Main {
         //System.out.println("\u2680");
         Cup myCup = new Cup();
         Scanner scanner = new Scanner(System.in);
-        int input;
+        //int input;
+        String input;
 
         myCup.roll();
         System.out.println(myCup.displayCup());
+        System.out.println("Which dice do you want to re-roll (1-5");
+        input = scanner.nextLine();
 
-        System.out.println("Which die do you want to re-roll (1-5");
-
-        input = scanner.nextInt();
-        myCup.roll(input -1);
+        myCup.roll(myCup.parseSelections(input));
         System.out.println(myCup.displayCup());
     }
 }
